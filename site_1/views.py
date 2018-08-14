dfc535759fbe709b6299847818622b6e2927c30f
Henrique Cordeiro <ofertasjacontato@gmail.com>
2018-08-14 20:24:04 -0300

    Mudanças com HTML 1

--- site_1/views.py
+++ site_1/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Produto
 
 def listar_ofertas(request):
-	return render(request, 'site_1/listar_ofertas.html', {})
+	ofertas = Produto.objects.filter(data_publicacao__lte=timezone.now()).order_by('data_publicacao')
+	return render(request, 'site_1/listar_ofertas.html', {'ofertas': ofertas})
