8f9c788a3596eab439bf6ea075587a484ad3f1e6
Henrique Cordeiro <ofertasjacontato@gmail.com>
2018-08-15 03:17:32 -0300

    Commit quase pronto

--- site_1/urls.py
+++ site_1/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-	path('', views.listar_ofertas, name='listar_ofertas'),
+	path('', views.index, name='index'),
 ]
--- site_1/views.py
+++ site_1/views.py
@@ -4,4 +4,4 @@ from .models import Produto
 
 def listar_ofertas(request):
 	ofertas = Produto.objects.filter(data_publicacao__lte=timezone.now()).order_by('data_publicacao')
-	return render(request, 'site_1/listar_ofertas.html', {'ofertas': ofertas})
+	return render(request, 'site_1/index.html', {'ofertas': ofertas})
